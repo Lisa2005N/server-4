302ad70960d65809ad28ae90558bc841fedf1a88
Lisa2005N <212870182+Lisa2005N@users.noreply.github.com>
2025-10-24 12:12:35 +0900

    変更

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // 自分で一から書いてみよう
+import { Hono } from 'jsr:@hono/hono';
+import { serveStatic } from 'jsr:@hono/hono/deno';
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }));
+
+app.get('/api', async (c) => {
+  const res = await fetch('https://randomuser.me/api/');
+  // console.log('in the server side.');
+  return c.json({ message: data });
+});
+
+Deno.serve(app.fetch);
